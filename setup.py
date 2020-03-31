60e4c33e3926652a1fe1217dcfbdb33077455fc5
Dustin Lo <dustinlo.pcc@gmail.com>
2020-03-30 18:38:10 -0700

    Develop es7 (#2) (#3)
    * upgrade elasticsearch python library
    
    * formatting setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'elasticsearch>=1.0.0,<2.0.0', 'requests>=2.18.4', 'simplejson>=3.11.1'
+        'elasticsearch>=7.0.0,<8.0.0',
+        'elasticsearch-dsl>=7.0.0,<8.0.0',
+        'requests>=2.18.4',
+        'simplejson>=3.11.1'
     ]
 )
