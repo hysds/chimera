c2955745a7c214acda566951e2fdb9020b583d9f
Gerald Manipon <pymonger@gmail.com>
2020-04-14 11:58:27 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ adaptation_path = "folder/"
 
 setup(
     name='chimera',
-    version='1.0.0',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=[
         'elasticsearch>=7.0.0,<8.0.0',
