2134747d2508bc7d97f2578cf8d1b401d76f49cf
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2020-07-30 13:21:27 -0700

    HC-245: Add capability to run the PGE step inline on the worker (#6)
    * HC-245: Add capability to run the PGE step inline on the worker
    executing the chimera workflow
    
    * Check for empty CHIMERA config if needed
    
    * calculate localized hash only when using the parallel binding
    
    * fix return value of construct_params
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- chimera/pge_job_submitter.py
+++ chimera/pge_job_submitter.py
@@ -23,18 +23,26 @@ class PgeJobSubmitter(object):
             self._context = json.load(open(context, 'r'))
         logger.debug("Loaded context file: {}".format(json.dumps(self._context)))
 
+        # This is intended to represent the top level working directory of the job. It's assumed to be at the same
+        # level as the given context file.
+        self._base_work_dir = os.path.dirname(os.path.abspath(context))
+
         # load pge config file
         self._pge_config = load_config(pge_config_file)
         logger.debug("Loaded PGE config file: {}".format(json.dumps(self._pge_config)))
+
+        self._wuid = wuid
+        self._job_num = job_num
 
         # load Settings file
         try:
             if settings_file:
                 settings_file = os.path.abspath(os.path.normpath(settings_file))
                 self._settings = YamlConf(settings_file).cfg
-                self._chimera_config = self._settings.get("CHIMERA")
-                if not self._chimera_config:
-                    raise RuntimeError("Must specify a CHIMERA area in {}".format(settings_file))
+                self._chimera_config = self._settings.get("CHIMERA", None)
+                if self._wuid and self._job_num:
+                    if not self._chimera_config:
+                        raise RuntimeError("Must specify a CHIMERA area in {}".format(settings_file))
         except Exception as e:
             if settings_file:
                 file_name = settings_file
@@ -43,8 +51,6 @@ class PgeJobSubmitter(object):
             raise RuntimeError("Could not read settings file '{}': {}".format(file_name, e))
 
         self._run_config = run_config
-        self._wuid = wuid
-        self._job_num = job_num
 
     def get_input_file_name(self, input_file_key=None):
         """
@@ -87,14 +93,6 @@ class PgeJobSubmitter(object):
 
         :return:
         """
-        # get HySDS job type and queue information
-        job_name = self._chimera_config.get(chimera_const.JOB_TYPES).get(self._pge_config.get(chimera_const.PGE_NAME))
-        if chimera_const.RELEASE_VERSION in self._context:
-            release_version = self._context[chimera_const.RELEASE_VERSION]
-        else:
-            release_version = self._context.get('container_specification').get('version')
-        job_type = job_name + ":" + release_version
-
         try:
             localize_urls = self.get_localize_urls(self._run_config.get(chimera_const.LOCALIZE))
         except Exception:
@@ -109,8 +107,7 @@ class PgeJobSubmitter(object):
             "simulate_outputs": self._run_config[chimera_const.SIMULATE_OUTPUTS]
         }
 
-        localize_hash = self.get_payload_hash(job_type)
-        return job_params, localize_hash
+        return job_params
 
     def get_payload_hash(self, job_type):
         """
@@ -167,45 +164,56 @@ class PgeJobSubmitter(object):
         return job
 
     def submit_job(self):
-        # get HySDS job type and queue information
-        job_name = self._chimera_config.get(chimera_const.JOB_TYPES).get(self._pge_config.get(chimera_const.PGE_NAME))
-        job_queue = self._chimera_config.get(chimera_const.JOB_QUEUES).get(
-            self._pge_config.get(chimera_const.PGE_NAME))
-
-        if chimera_const.RELEASE_VERSION in self._context:
-            release_version = self._context[chimera_const.RELEASE_VERSION]
-        else:
-            release_version = self._context.get('container_specification').get('version')
-
-        job_type = job_name + ":" + release_version
-
-        # Find what the primary input is to the job
-        # input_file_key = self._pge_config.get(chimera_const.PRIMARY_INPUT, None)
-        # dataset_id = self.get_input_file_name(input_file_key)
-
-        # Nominally, the primary input is used as part of the job name. If we wanted to set something else in the job
-        # name, look to see if the pge_job_name field is specified in the run_config
-        dataset_id = self._run_config.get("pge_job_name", None)
-
-        if dataset_id:
-            logger.info("dataset_id is set to {}".format(dataset_id))
-
-        # Set the sciflo fields wuid and job num
-        # these are internally passed context information available in sciflo processes
-        if self._wuid is None or self._job_num is None:
-            raise RuntimeError("Need to specify workunit id and job num.")
-
         if not isinstance(self._run_config, dict):
             raise RuntimeError("The output from input preprocessor is not a dictionary")
 
-        params, localize_hash = self.construct_params()
-        job_json = self.construct_job_payload(params, dataset_id=dataset_id, pge_config=self._pge_config,
-                                              job_type=job_type, job_queue=job_queue, payload_hash=localize_hash)
+        params = self.construct_params()
 
-        job_json['payload']['_sciflo_wuid'] = self._wuid
-        job_json['payload']['_sciflo_job_num'] = self._job_num
+        # If wuid and job_num are not null, it is implied that we need to do job submission. In that case, we need to
+        # construct the job payload.
+        if self._wuid and self._job_num:
+            # get HySDS job type and queue information
+            job_name = self._chimera_config.get(chimera_const.JOB_TYPES).get(
+                self._pge_config.get(chimera_const.PGE_NAME))
+            job_queue = self._chimera_config.get(chimera_const.JOB_QUEUES).get(
+                self._pge_config.get(chimera_const.PGE_NAME))
 
-        logger.debug("Resolved Job JSON: {}".format(json.dumps(job_json)))
+            if chimera_const.RELEASE_VERSION in self._context:
+                release_version = self._context[chimera_const.RELEASE_VERSION]
+            else:
+                release_version = self._context.get('container_specification').get('version')
+
+            job_type = job_name + ":" + release_version
+
+            localize_hash = self.get_payload_hash(job_type)
+
+            # Find what the primary input is to the job
+            # input_file_key = self._pge_config.get(chimera_const.PRIMARY_INPUT, None)
+            # dataset_id = self.get_input_file_name(input_file_key)
+
+            # Nominally, the primary input is used as part of the job name. If we wanted to set something else in the
+            # job
+            # name, look to see if the pge_job_name field is specified in the run_config
+            dataset_id = self._run_config.get("pge_job_name", None)
+
+            if dataset_id:
+                logger.info("dataset_id is set to {}".format(dataset_id))
+
+            job_json = self.construct_job_payload(params, dataset_id=dataset_id, pge_config=self._pge_config,
+                                                  job_type=job_type, job_queue=job_queue, payload_hash=localize_hash)
+            # Set the sciflo fields wuid and job num
+            # these are internally passed context information available in sciflo processes
+            job_json['payload']['_sciflo_wuid'] = self._wuid
+            job_json['payload']['_sciflo_job_num'] = self._job_num
+
+            logger.debug("Resolved Job JSON: {}".format(json.dumps(job_json)))
+        else:
+            # If we're running inline, we will set the params as the job_json
+            job_json = params
+            # We also need to get the job_specification from _context.json as that contains dependency image
+            # information, if specified
+            if "job_specification" in self._context:
+                job_json["job_specification"] = self._context["job_specification"]
 
         job_json = self.perform_adaptation_tasks(job_json)
 
