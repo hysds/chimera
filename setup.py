829dda61e2d2f6d2aeaeec63997e41bdb932ab6d
Gerald Manipon <pymonger@gmail.com>
2021-01-19 16:12:33 -0800

    HC-317: various improvements to the chimera precondition evaluator's runconfig preparation (#13)
    * various improvements to the chimera precondition evaluator's runconfig preparation
    
    * make empty field identifier overridable in PGE config
    
    * bump version
    
    * restore optional_fields feature
    
    * Update chimera/precondition_evaluator.py
    
    Co-authored-by: Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
    
    * Update chimera/precondition_evaluator.py
    
    Co-authored-by: Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
    
    Co-authored-by: Michael Cayanan <42812746+mcayanan@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ adaptation_path = "folder/"
 
 setup(
     name='chimera',
-    version='2.1.3',
+    version='2.1.4',
     packages=find_packages(),
     install_requires=[
         'elasticsearch>=7.0.0,<8.0.0',
