4386432c7a44505c17c2e1d2142f46e1049b3fc4
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-10-08 10:25:43 -0700

    updates to resolve new pylint checks

--- tests/test_input_preprocessor.py
+++ tests/test_input_preprocessor.py
@@ -1,5 +1,5 @@
 import simplejson
-from nisar_chimera import input_preprocessor as ipp
+from chimera import input_preprocessor as ipp
 
 if __name__ == '__main__':
     sys_config = "../nisar_chimera/configs/sys.config.json"
@@ -7,16 +7,17 @@ if __name__ == '__main__':
 
     # For testing without sfl_exec L0A
     context = simplejson.load(
-        open("test-files/L0A_sfcontext.json", 'r'))
-    ipp_def_filepath = "../nisar_chimera/configs/precondition_definition.yaml"
-    pge_config = "../nisar_chimera/configs/pge_configs/PGE_L0A.yaml"
-    settings_file = '../../nisar-pcm/conf/settings.yaml'
+        open("test-files/L0B_datatake.json", 'r'))
+    #ipp_def_filepath = "../nisar_chimera/configs/precondition_definition.yaml"
+    chimera_config = "/Users/mcayanan/git/nisar-pcm/nisar_chimera/configs/chimera_config.yaml"
+    pge_config = "/Users/mcayanan/git/nisar-pcm/nisar_chimera/configs/pge_configs/PGE_L0B.yaml"
+    settings_file = '/Users/mcayanan/git/nisar-pcm/conf/settings.yaml'
     test_configs.append((context, pge_config))
 
     # context = process_for_l0b_radiometer(context, simplejson.load(open(pge_config, 'r')))
     # Loop through all test configs
     for context, pge_config in test_configs:
-        payload = ipp.process(sf_context=context, ipp_def_filepath=ipp_def_filepath, pge_config_filepath=pge_config,
-                              sys_config_file=sys_config, settings_file=settings_file, testmode=True)
+        payload = ipp.process(sf_context=context, chimera_config_file=chimera_config, pge_config_filepath=pge_config,
+                              settings_file=settings_file)
 
-    print(simplejson.dumps(payload, indent=2))
+        print(simplejson.dumps(payload, indent=2))
