d61d662bdfe0e402593e03ce33728db1494eebe0
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2024-10-14 09:08:31 -0700

    HC-522-update: Updates to support moving away from OPS user (#22)
    * HC-522-update: Use system cp call to update SELinux context label
    
    * bump version
    
    * updates to resolve new pylint checks
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ adaptation_path = "folder/"
 
 setup(
     name='chimera',
-    version='2.2.3',
+    version='2.2.4',
     packages=find_packages(),
     install_requires=[
         'elasticsearch>=7.0.0,<7.14.0',
